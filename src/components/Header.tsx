0e5d7d64534a1f7e9c1beedf1a9aa5ddd130a11c
renanmartins1813 <renanmartins1813@gmail.com>
2025-08-07 01:25:27 -0500

    b

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import H1 from "./H1";
 export default function Header() {
     return (
         <header>
-            <Background />
+            <BackgroundImage/>
             <H1 />
         </header>
     )
